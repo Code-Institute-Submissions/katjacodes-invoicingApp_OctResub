ea51fceae8db2b25f20e0f94755751a72bcd9a71
katjacodes <katjacodes@gmail.com>
2021-10-08 02:45:40 +0000

    wire up client info edit button

--- app.py
+++ app.py
@@ -106,6 +106,13 @@ def add_client():
     return render_template("addClient.html", categories=categories)
 
 
+@app.route("/edit_clientInfo/<clientInfo_id>", methods=["GET", "POST"])
+def edit_clientInfo(clientInfo_id):
+    clientInfo = mongo.db.clientInfo.find_one({"_id": ObjectId(clientInfo_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("editClient.html", clientInfo=clientInfo, categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
